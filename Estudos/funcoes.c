beb7af8e000a0550ebc652eb86a1ce658bd76484
Marcelo Manzo <marcelohmanzo@gmail.com>
2026-08-11 07:27:03 -0300

    Add string manipulation functions documentation

---
+++ Estudos/funcoes.c
@@ -0,0 +1,5 @@
+strlen() -- retorna o tamanho da string. EX: tamanho = strlen(texto) -- tamanho = 3  // texto = ['abc']
+strcpy() -- copia uma string para a outra. EX: (texto2,texto) -- texto2 = texto // texto2 = ['abc']
+sltcat() -- realiza a juncao de duas strings. Ex: sltcat(texto,texto2) = ['abcabc']
+strcmp() -- compara duas strings. ex: igual = strcmp(texto,texto2) -- igual = true //['abc'] == ['abc']
+strncmp() -- compara n primeiros n digitos de duas strings. EX igual = strncmp(texto, texto2, 3) //retorna true
