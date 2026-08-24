32d1f811362649f9f85ffdeffe6eb98cbf9d4537
Marcelo Manzo <marcelohmanzo@gmail.com>
2026-08-24 07:19:21 -0300

    Fix syntax errors and improve comments in Exemplo_Ponteiros.c

--- Estudos/ponteiros/Exemplo_Ponteiros.c
+++ Estudos/ponteiros/Exemplo_Ponteiros.c
@@ -10,3 +10,12 @@ int main()
     *q = 21;  o endereco de q (mesmo endereco de P tb) recebe o valor 21. logo, x passa a ser 21;
     printf("%d, %d",x, y);printa:  21, 7;
 }
+
+int main()
+{
+    int v[5] = {1,2,3,4,5}
+    int *p = v;
+    p esta apontando para v[0], ou seja,  1.
+    p++;
+    agora p esta apontando para v[1] ou seja, 2
+}
