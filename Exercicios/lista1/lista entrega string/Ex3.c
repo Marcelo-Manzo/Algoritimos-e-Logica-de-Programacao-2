1bce566ba231b8c31460fe92756e329b774e788d
Marcelo Manzo <marcelohmanzo@gmail.com>
2026-08-24 14:23:15 -0300

    Refactor Ex3.c to improve phrase reading logic and enhance clarity in comments

--- Exercicios/lista1/lista entrega string/Ex3.c
+++ Exercicios/lista1/lista entrega string/Ex3.c
@@ -6,61 +6,59 @@ int main() {
     scanf("%d", &n);
 
     // Dicionário com tamanho máximo de 50 letras por palavra
-    char palavrasIngles[n][50];
-    char setas[n][50];
-    char traducao[n][50];
+    char palavrasIngles[n][50], setas[n][50],traducao[n][50], frase[500];
+    int continuar_leitura = 1;
 
     // 1. Lendo o dicionário
     for (int i = 0; i < n; i++) {
         scanf("%s %s %s", palavrasIngles[i], setas[i], traducao[i]);
     }
 
-    char frase[500];
-
-    // 2. Lendo a frase inteira até o "Enter"
-    while (scanf(" %[^\n]", frase) == 1) {
+    // 2. Lendo a frase inteira até o "Enter" e enquanto não for digitado "*"
+    while (continuar_leitura && scanf(" %[^\n]", frase) == 1) {
         
-        // Se a frase for apenas o asterisco, o programa para
+        // Se a frase for apenas o asterisco, altera a variável para encerrar o programa
         if (strcmp(frase, "*") == 0) {
-            break;
-        }
-        
-        char palavraAtual[50];
-        int tamanho_palavra = 0;
-        int tam_frase = strlen(frase);
+            continuar_leitura = 0;
+        } 
+        else {
+            char palavraAtual[50];
+            int tamanho_palavra = 0;
+            int tam_frase = strlen(frase);
 
-        for (int j = 0; j <= tam_frase; j++) {
-            
-            if (frase[j] == ' ' || frase[j] == '\0') {
+            for (int j = 0; j <= tam_frase; j++) {
                 
-                if (tamanho_palavra > 0) {
-                    palavraAtual[tamanho_palavra] = '\0';
+                if (frase[j] == ' ' || frase[j] == '\0') {
+                    
+                    if (tamanho_palavra > 0) {
+                        palavraAtual[tamanho_palavra] = '\0';
 
-                    int achou = 0;
-                    for (int z = 0; z < n; z++) {
-                        if (strcmp(palavraAtual, palavrasIngles[z]) == 0) {
-                            printf("%s", traducao[z]);
-                            achou = 1;
-                            break;
+                        int achou = 0;
+                        // Substituído o break adicionando !achou na condição do for
+                        for (int z = 0; z < n && !achou; z++) {
+                            if (strcmp(palavraAtual, palavrasIngles[z]) == 0) {
+                                printf("%s", traducao[z]);
+                                achou = 1;
+                            }
                         }
+                        if (!achou) {
+                            printf("%s", palavraAtual);
+                        }
+                        // Se nao for o ultimo caractere da frase, imprime o espaco
+                        if (frase[j] == ' ') {
+                            printf(" ");
+                        }
+                        tamanho_palavra = 0; // Reinicia para a próxima palavra
                     }
-                    if (!achou) {
-                        printf("%s", palavraAtual);
-                    }
-                    // Se nao for o ultimo caractere da frase, imprime o espaco
-                    if (frase[j] == ' ') {
-                        printf(" ");
-                    }
-                    tamanho_palavra = 0; // Reinicia para a próxima palavra
+                } 
+                // Se for um caractere normal
+                else {
+                    palavraAtual[tamanho_palavra] = frase[j];
+                    tamanho_palavra++;
                 }
-            } 
-            // Se for um caractere normal
-            else {
-                palavraAtual[tamanho_palavra] = frase[j];
-                tamanho_palavra++;
             }
+            printf("\n"); 
         }
-        printf("\n"); 
     }
 
     return 0;
